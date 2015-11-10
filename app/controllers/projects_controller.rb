65a636b5dadb7a9fe4d640d9b33686cc79319845
Jodie Alaine Parker <me@jodiealaine.com>
2015-11-10 23:28:53 +0000

    Add ability to create new projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,7 @@
 class ProjectsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
+	before_action :set_project, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@projects = Project.all
 		@displayed_projects = Project.take(4)
@@ -12,5 +15,29 @@ class ProjectsController < ApplicationController
 	end
 
 	def edit
+	end
+
+	def create
+		@project = current_user.projects.build(project_params)
+
+		respond_to do |format|
+			if @project.save
+				format.html { redirect_to @project, notice: "Project was succesfully created!"}
+				format.json { render :show, status: :ok, location: @project }
+			else
+				format.html { render :new }
+				format.json { render json: @project.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	private
+
+	def set_project
+		@project = Project.friendly.find(params[:id])
+	end
+	
+	def project_params
+		params.require(:project).permit(:name, :short_description, :description, :goal, :image_url, :expiration_date)
 	end
 end
